a99aa61cfe8eb4a84755452f7332f910e4494971
Kemal Maulana <kemalmaulana19@gmail.com>
2022-12-29 01:57:48 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kemal Maulana on 29/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
